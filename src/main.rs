3e8547a5dd279274e88dd52528adb700685c0433
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-05-29 12:47:21 +0200

    step 3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod pong;
+mod systems;
 
 use crate::pong::Pong;
 
 use amethyst::{
     prelude::*,
     core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
@@ -19,8 +21,15 @@ fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
 
     let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
@@ -29,7 +38,7 @@ fn main() -> amethyst::Result<()> {
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?
-        .with_bundle(TransformBundle::new())?;
+        ;
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
     game.run();
--- src/pong.rs
+++ src/pong.rs
@@ -31,7 +31,7 @@ impl SimpleState for Pong {
 
         let sprite_sheet_handle = load_sprite_sheet(world);
 
-        world.register::<Paddle>();
+        // world.register::<Paddle>();
 
         initialize_paddles(world, sprite_sheet_handle);
         initialize_camera(world);
