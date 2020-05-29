73b9712e3670e903e51b67bf9006800f501ecb03
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-05-29 14:05:50 +0200

    move ball

--- src/pong.rs
+++ src/pong.rs
@@ -12,6 +12,9 @@ pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
 pub const PADDLE_WIDTH: f32 = 4.0;
+pub const BALL_VELOCITY_X: f32 = 75.0;
+pub const BALL_VELOCITY_Y: f32 = 50.0;
+pub const BALL_RADIUS: f32 = 2.0;
 
 #[derive(PartialEq, Eq)]
 pub enum Side {
@@ -25,6 +28,15 @@ pub struct Paddle {
     pub height: f32,
 }
 
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Component for Ball {
+    type Storage = DenseVecStorage<Self>;
+}
+
 impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
@@ -32,7 +44,9 @@ impl SimpleState for Pong {
         let sprite_sheet_handle = load_sprite_sheet(world);
 
         // world.register::<Paddle>();
+        world.register::<Ball>();
 
+        initialise_ball(world, sprite_sheet_handle.clone());
         initialize_paddles(world, sprite_sheet_handle);
         initialize_camera(world);
     }
@@ -50,6 +64,29 @@ impl Paddle {
 
 impl Component for Paddle {
     type Storage = DenseVecStorage<Self>;
+}
+
+/// Initialises one ball in the middle-ish of the arena.
+fn initialise_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    // Create the translation.
+    let mut local_transform = Transform::default();
+    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    // Assign the sprite for the ball
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 1, // ball is the second sprite on the sprite sheet
+    };
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
+        .build();
 }
 
 fn initialize_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
